0a87010b6a22cfa60fbd9eea0e01d31807f3bc9f
Ryan Ho <ryanhohoho6@gmail.com>
2024-08-22 00:55:52 -0400

    inefficiently added card info as well

--- webscraper.py
+++ webscraper.py
@@ -18,6 +18,36 @@ del rows[0]
 for i in rows:
     code = i.find_all("td")[0]
     name = i.find_all("td")[1]
-    print(code.string + " | " + name.string)
+    link = name.find("a")
+    #print("https://cardfight.fandom.com" + link.get("href"))
+    print(code.string + " | " + name.string + " | ", end='')
+
+    subUrl = "https://cardfight.fandom.com" + link.get("href")
+    subResult = requests.get(subUrl)
+    cardPage = BeautifulSoup(subResult.text, "html.parser")
+
+    infoString = ""
+    info = cardPage.find("div", {"class": "info-main"})
+    #print(info)
+
+    type = info.find(string="Card Type")
+    #Found the Clan row, tooks its parent's parent's child (aka sibling), found the a link, then the string associated with the a link
+    if (type != None):    
+        typeInfo = (((type.parent).parent).find_all("td")[1]).find("a")
+        infoString += typeInfo.string + " | "
+    else:
+        infoString += "Normal Unit" + " | "
+
+    clan = info.find(string="Clan")
+    #Found the Clan row, tooks its parent's parent's child (aka sibling), found the a link, then the string associated with the a link
+    clanInfo = (((clan.parent).parent).find_all("td")[1]).find("a")
+    infoString += clanInfo.string + " | "
+
+    race = info.find(string="Race")
+    #Found the Clan row, tooks its parent's parent's child (aka sibling), found the a link, then the string associated with the a link
+    raceInfo = (((race.parent).parent).find_all("td")[1]).find("a")
+    infoString += raceInfo.string + " | "
+
+    print(infoString)
 
 #print(rows.prettify())
