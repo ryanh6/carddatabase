6f4716759acda6016e053a846063ccfc0364766c
Ryan Ho <ryanhohoho6@gmail.com>
2024-09-04 15:52:52 -0400

    testing with pandas once again

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,13 @@
+# Pip Installs Include...
+# - pip install beautifulsoup4
+# - pip install requests
+# - pip install pandas
+# - pip install lxml
+# - pip install openpyxl
+
 from bs4 import BeautifulSoup
+from io import StringIO
+import pandas as pd
 import requests
 
 def fullImageLink(name):
@@ -21,3 +30,10 @@ def readCardInfo(pageURL):
 def readSetInfo(pageURL):
     setRequest = requests.get(pageURL)
     setPage = BeautifulSoup(setRequest.text, "html.parser")
+
+    setList = setPage.find("table")
+    setTable = pd.read_html(StringIO(str(setList)))[0]
+
+    print(setTable.to_string())
+
+readSetInfo("https://cardfight.fandom.com/wiki/Booster_Set_1:_Descent_of_the_King_of_Knights")
