f19f3253cce3846531d7a292e33227809e0f16c4
Ryan Ho <ryanhohoho6@gmail.com>
2024-11-23 00:27:17 -0500

    readded attributes

--- cfvWebscraper.py
+++ cfvWebscraper.py
@@ -5,18 +5,73 @@ import requests
 #     galleryRequest = requests.get(cardGalleryLink)
 #     galleryPage = BeautifulSoup(galleryRequest.text, "html.parser")
 
-#     # sections = galleryPage.find("div", {"id": "gallery-0"})
-
-#     artworks = galleryPage.find_all("div", {"class": "wikia-gallery-item"})
-#     print(artworks)
+#     # artworks = galleryPage.find_all("div", {"class": "wikia-gallery-item"})
+#     artworks = galleryPage.find_all("div", string = "Full Art")
 
 #     for element in artworks:
 #         # parent = element.parent
-#         # next = parent.previous_sibling
-#         image = element.find("img")
+#         next = element.previous_sibling
+#         image = next.find("img")
 #         finalImage = image.get("src")
 #         print(finalImage)
 #         print()
+
+def findGiftMarker(targetClan):
+    clanDictionary = {"Accel": ["Aqua Force", "Gold Paladin", "Great Nature", "Murakumo",
+                                "Narukami", "Nova Grappler", "Pale Moon", "Tachikaze"],
+                      "Force": ["Bermuda Triangle", "Dimension Police", "Gear Chronicle", "Genesis", "Kagero", 
+                                "Link Joker", "Neo Nectar", "Royal Paladin", "Shadow Paladin", "Spike Brothers"],
+                      "Protect": ["Angel Feathers", "Dark Irregulars", "Granblue",
+                                  "Megacolony", "Nubatama", "Oracle Think Tank"]}
+    
+    for giftMarker in clanDictionary:
+        for clan in clanDictionary[giftMarker]:
+            if (clan == targetClan):
+                return giftMarker
+
+def addAttributes(dictionary):
+    if (dictionary.get("Card Type") == None):
+        dictionary.update({"Card Type": "Normal Unit"})
+
+    if (dictionary.get("Illust") == None):
+        dictionary.update({"Artist": dictionary.get("Design /  Illust")})
+    else:
+        dictionary.update({"Artist": dictionary.get("Illust")})
+
+    return dictionary
+
+def deleteAttributes(dictionary):
+    toRemove = ["Kanji", "Kana", "Phonetic", "Thai", "Italian", "Korean", 
+                "Grade / Skill", "Illust", "Design /  Illust", "Translation"]
+    
+    for key in toRemove:
+        dictionary.pop(key, None)
+
+    return dictionary
+
+def editAttributes(dictionary):
+    if (dictionary.get("Grade / Skill") != None):
+        splitGrade = dictionary.get("Grade / Skill").split(" / ")
+
+        dictionary.update({"Grade": (splitGrade[0]).strip()})
+        if (len(splitGrade) > 1):
+            dictionary.update({"Skill": (splitGrade[1]).strip()})
+
+    if (dictionary.get("Imaginary Gift") != None):
+        giftMarker = findGiftMarker(dictionary.get("Clan"))
+        dictionary.update({"Imaginary Gift": giftMarker})
+
+    if (dictionary.get("Trigger Effect") != None):
+        triggerEffect = dictionary.get("Trigger Effect").split(" / ")[0]
+        dictionary.update({"Trigger Effect": triggerEffect})
+
+    return dictionary
+
+def editDictionary(dictionary):
+    dictionary = addAttributes(dictionary)
+    dictionary = editAttributes(dictionary)
+    dictionary = deleteAttributes(dictionary)
+    return dictionary
 
 def readCardEffect(pageData):
     try:
@@ -26,7 +81,7 @@ def readCardEffect(pageData):
     except:
         return ({"Card Effect(s)": "-"})
 
-def cfvReadCard(pageURL, dictionary):
+def cfvReadCard(pageURL):
     try:
         cardRequest = requests.get(pageURL)
         cardPage = BeautifulSoup(cardRequest.text, "html.parser")
@@ -36,6 +91,7 @@ def cfvReadCard(pageURL, dictionary):
     except:
         return
     
+    dictionary = {}
     for index in range(0, len(attributes)):
         if (index % 2 == 0):
             title = (attributes[index].text).strip()
@@ -43,15 +99,13 @@ def cfvReadCard(pageURL, dictionary):
             dictionary.update({title: trait})
     
     dictionary.update(readCardEffect(cardPage))
+
+    editDictionary(dictionary)
+
     print(dictionary)
 
 def readSet():
-    dictionary = {}
-    dictionary.update({"Card ID": "BT01/001"})
-    dictionary.update({"Set Name": "Descent of the King of Knights"})
-    dictionary.update({"Release Date": "12/03/2011"})
-
-    cfvReadCard("https://cardfight.fandom.com/wiki/King_of_Knights,_Alfred", dictionary)
+    cfvReadCard("https://cardfight.fandom.com/wiki/King_of_Knights,_Alfred")
 
 readSet()
 # cfvCardArtworks("https://cardfight.fandom.com/wiki/Card_Gallery:King_of_Knights,_Alfred")
