4f38d42fa1479e5e4874f8ab24148cc5e25d3774
Ryan Ho <ryanhohoho6@gmail.com>
2024-11-26 23:43:11 -0500

    commented out a few features that are not ready yet

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import openpyxl
 
+def sortDatabase(dataframe):
+    return dataframe.sort_values(["Release Date", "Card ID"])
+    
+def removeDuplicates(dataframe):
+    return dataframe.drop_duplicates("Card ID")
+
 def createExcel(fileName, sheetName, columnNames):
     table = pd.DataFrame(columns = columnNames)
     table.to_excel(fileName, sheet_name = sheetName, index = False, na_rep = "-")
@@ -12,6 +18,11 @@ def updateExcel(fileName, sheetName, cardList):
     combined = pd.concat([table, newRows])
     combined.to_excel(fileName, sheet_name = sheetName, index = False, na_rep = "-")
 
+# def formatDatabase(fileName, sheetName):
+#     table = pd.read_excel(fileName)
+#     table = removeDuplicates(table)
+#     table = sortDatabase(table)
+#     table.to_excel(fileName, sheet_name = sheetName, index = False, na_rep = "-")
 
 def cfvMain():
     columnNames = ["Card ID", "Name", "Card Type", "Grade", "Skill",
