6fd38a6d7ecd7f3964982b555b00ccffcc296f54
Ryan Ho <ryanhohoho6@gmail.com>
2024-12-15 22:48:32 -0500

    apparently unsaved issues

--- database.py
+++ database.py
@@ -23,14 +23,3 @@ def updateExcel(fileName, sheetName, cardList):
 #     table = removeDuplicates(table)
 #     table = sortDatabase(table)
 #     table.to_excel(fileName, sheet_name = sheetName, index = False, na_rep = "-")
-
-def cfvMain():
-    columnNames = ["Card ID", "Name", "Card Type", "Grade", "Skill",
-                    "Imaginary Gift", "Special Icon", "Trigger Effect",
-                    "Power", "Shield", "Critical", "Nation", "Clan", "Race", 
-                    "Series", "Format", "Artist", "Card Effect(s)", 
-                    "Set ID", "Set Name", "Rarity", "Card Art(s)", "Release Date", 
-                    "Language", "Restrictions", "Full Art(s)"]
-    createExcel("cfvdatabase.xlsx", "All Cards", columnNames)
-
-cfvMain()
