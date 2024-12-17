97dc1e533cfbb584ae4a252aab4040c0fbef32bd
Ryan Ho <ryanhohoho6@gmail.com>
2024-12-17 14:04:28 -0500

    working on issue where JP grabs all codes instead of just jp codes

--- cfvWebscraper.py
+++ cfvWebscraper.py
@@ -1,12 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
 
+import pandas as pd
+
 def readFullArts(galleryData):
     return galleryData.find_all("div", {"class": "wikia-gallery-item"})
 
 def createGalleryLink(pageURL):
     cardName = pageURL.split("/")[-1]
     return "https://cardfight.fandom.com/wiki/Card_Gallery:" + cardName
+
+def rescaleImage(image):
+    newString = ""
+    splitString = image.split("/")
+
+    for section in splitString:
+        if (section == "scale-to-width-down"):
+            break
+        else:
+            newString += section + "/"
+    
+    return newString + "?" + image.split("?")[-1]
+
+def searchArtworks(artworks, keyword):
+    imagesString = ""
+
+    for element in artworks:
+        if (keyword in element.text and "IT" not in element.text):
+            try:
+                print(element.text)
+                image = element.find("img")
+                imageSource = image.get("src")
+                scaledImage = rescaleImage(imageSource)
+                
+                imagesString += scaledImage + ", "
+            except:
+                continue
+
+    if (imagesString == ""):
+        return ({"Full Art(s)": "-"})
+    
+    return ({"Full Art(s)": imagesString[0:-2]})
 
 def readPage(pageURL):
     pageRequest = requests.get(pageURL)
@@ -100,6 +134,22 @@ def editSetID(dictionary):
     code = dictionary.get("Card ID")
     return ({"Set ID": code.split("/")[0]})
 
+def readCardSets(pageData):
+    cardSets = pageData.find("table", {"class": "sets"})
+    setsDescription = (cardSets.find("td")).find_all("li")
+
+    codeList = []
+    for set in setsDescription:
+        codes = set.get_text(separator = " - ")
+
+        splitedCodes = codes.split(" - ")
+        splitedCodes = splitedCodes[2:]
+
+        for code in splitedCodes:
+            codeList.append(code)
+
+    return codeList
+
 def readCardEffect(pageData):
     try:
         cardEffect = pageData.find("table", {"class": "effect"})
@@ -109,9 +159,14 @@ def readCardEffect(pageData):
         return ({"Card Effect(s)": "-"})
 
 def readGiftMarker(pageData):
-    print("hi")
-    giftMarker = pageData.find_all()
-    print(giftMarker)
+    if (pageData.find("a", {"title": "Imaginary Gift/Force"}) != None):
+        return ({"Imaginary Gift": "Force"})
+    elif (pageData.find("a", {"title": "Imaginary Gift/Accel"}) != None):
+        return ({"Imaginary Gift": "Accel"})
+    elif (pageData.find("a", {"title": "Imaginary Gift/Protect"}) != None):
+        return ({"Imaginary Gift": "Protect"})
+    
+    return ({"Imaginary Gift": "-"})
 
 def addAttributes(dictionary, attributes):
     for index in range(0, len(attributes)):
@@ -137,22 +192,66 @@ def createBasicDictionary(cardPageData, artworksArray):
     cardDictionary = {}
     addAttributes(cardDictionary, attributes)
     cardDictionary.update(readCardEffect(cardPageData))
-    # cardDictionary.update(readGiftMarker(cardMainInfo))
+    cardDictionary.update(readGiftMarker(cardMainInfo))
     cardDictionary.update(editCardType(cardDictionary))
     cardDictionary.update(editIllust(cardDictionary))
     cardDictionary.update(editGrade(cardDictionary))
     cardDictionary.update(editSkill(cardDictionary))
     cardDictionary.update(editTrigger(cardDictionary))
+    cardDictionary.update(searchArtworks(artworksArray, "Full Art"))
     deleteAttributes(cardDictionary)
 
     return cardDictionary
+
+def readTournamentStatus(cardPageData, dictionary):
+    language = dictionary.get("Language")
+
+    try:
+        status = cardPageData.find("table", {"class": "tourneystatus"})
+        region = status.find("td", string = language)
+        regulation = region.find_next("td")
+
+        tourneyStatus = regulation.text.strip()
+
+        if (tourneyStatus == "Unrestricted"):
+            return ({"Restrictions": tourneyStatus})
+        else:
+            tourneyStatus = ((regulation.find("a")).get("title"))
+            return ({"Restrictions": tourneyStatus})
+    except:
+        return
+
+def createCardList(cardPageData, basicCard, artworksArray, idArray):
+    cardList = []
+    
+    for element in idArray:
+        newCard = basicCard.copy()
+        newCard.update({"Card ID": element})
+        newCard.update(editSeries(newCard))
+        newCard.update(editFormat(newCard))
+        newCard.update(editRarity(newCard))
+        newCard.update(editLanguage(newCard))
+        newCard.update(editSetID(newCard))
+        newCard.update(editCardID(newCard))
+        newCard.update(readTournamentStatus(cardPageData, newCard))
+        # newCard.update(searchArtworks(artworksArray, newCard.get("Card ID")))
+        cardList.append(newCard)
+    
+    return cardList
 
 def cfvReadCard(pageURL):
     cardPageData = readPage(pageURL)
     galleryPageData = readPage(createGalleryLink(pageURL))
 
+    idArray = readCardSets(cardPageData)
     artworksArray = readFullArts(galleryPageData)
-    basicCard = createBasicDictionary(cardPageData, artworksArray)
-    print(basicCard)
 
-cfvReadCard("https://cardfight.fandom.com/wiki/King_of_Knights,_Alfred_(V_Series)")
+    basicCard = createBasicDictionary(cardPageData, artworksArray)
+    cardList = createCardList(cardPageData, basicCard, artworksArray, idArray)
+
+    return cardList
+
+data = cfvReadCard("https://cardfight.fandom.com/wiki/Blaster_Blade")
+print(data)
+# table = pd.DataFrame(data)
+# print(table)
