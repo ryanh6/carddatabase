488e4cd6da4143af5136a73cc609f1e4006acdb4
Ryan Ho <ryanhohoho6@gmail.com>
2024-12-22 00:58:47 -0500

    reshifting focus back to actually finishing the card data scraping first

--- cfvWebscraper.py
+++ cfvWebscraper.py
@@ -251,35 +251,35 @@ def cfvReadCard(pageURL):
 
     return cardList
 
-def cfvReadSets():
-    setsDataEN = readPage("https://en.cf-vanguard.com/products/")
+# def cfvReadSets():
+#     setsDataEN = readPage("https://en.cf-vanguard.com/products/")
 
-    productList = setsDataEN.find_all("div", {"class": "products-list"})
+#     productList = setsDataEN.find_all("div", {"class": "products-list"})
 
 
-    fullList = []
-    finalList = []
-    for item in productList:
-        itemInformation = item.find_all("div", {"class": "text"})
+#     fullList = []
+#     finalList = []
+#     for item in productList:
+#         itemInformation = item.find_all("div", {"class": "text"})
 
-        for element in itemInformation:
-            fullList.append(element)
+#         for element in itemInformation:
+#             fullList.append(element)
 
-    for element in fullList:
-        elementDictionary = {}
+#     for element in fullList:
+#         elementDictionary = {}
 
-        if (element.find("div", {"class": "category supply"}) != None):
-            continue
+#         if (element.find("div", {"class": "category supply"}) != None):
+#             continue
 
-        elementDictionary.update({"Name": (element.find("div", {"class": "title"})).text})
-        elementDictionary.update({"Release Date": (element.find("div", {"class": "release"})).text})
-        finalList.append(elementDictionary)
+#         elementDictionary.update({"Name": (element.find("div", {"class": "title"})).text})
+#         elementDictionary.update({"Release Date": (element.find("div", {"class": "release"})).text})
+#         finalList.append(elementDictionary)
 
-    for element in finalList:
-        print(element)
+#     for element in finalList:
+#         print(element)
 
 # data = cfvReadCard("https://cardfight.fandom.com/wiki/Blaster_Blade")
 # print(data)
 # table = pd.DataFrame(data)
 # print(table)
-cfvReadSets()
+# cfvReadSets()
