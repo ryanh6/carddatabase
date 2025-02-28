369f1f80b76182d255c75b923cb5ec386e175bd9
Ryan Ho <ryanhohoho6@gmail.com>
2025-02-28 14:57:02 -0500

    retrieve all info except for moves and evolutions

--- pkmnWebscraper.py
+++ pkmnWebscraper.py
@@ -3,12 +3,16 @@ import requests
 
 # Base Website Used: https://pkmncards.com/sets/
 
+# Helper Functions
+
 def readPage(pageURL):
     pageRequest = requests.get(pageURL)
     return BeautifulSoup(pageRequest.text, "html.parser")
 
 def decryptSymbol(pageContent):
     return pageContent.find("abbr", {"class": "ptcg-font ptcg-symbol-name"})
+
+# Retrieve Data Functions
 
 def retrieveName(pageContent):
     cardName = pageContent.find("span", {"title": "Name"})
@@ -47,6 +51,49 @@ def retrieveStage(pageContent):
         return ({"Stage": cardStage.text})
     return ({"Stage": "-"})
 
+def retrieveClass(pageContent):
+    cardClass = pageContent.find("span", {"title": "Is"})
+
+    if (cardClass != None):
+        return ({"Class": cardClass.text[4:]})
+    return ({"Class": "-"})
+
+def retrieveWeakness(pageContent):
+    section = pageContent.find("span", {"title": "Weakness"})
+
+    if (section == None):
+        return ({"Weakness": "-"})
+        
+    cardWeakness = decryptSymbol(section)
+    cardWeaknessModifier = section.find("span", {"title": "Weakness Modifier"})
+    
+    if (cardWeakness == None):
+        return ({"Weakness": "-"})
+    
+    return ({"Weakness": str(cardWeakness["title"]) + " " + str(cardWeaknessModifier.text)})
+
+def retrieveResistance(pageContent):
+    section = pageContent.find("span", {"title": "Resistance"})
+
+    if (section == None):
+        return ({"Resistance": "-"})
+        
+    cardResistance = decryptSymbol(section)
+    cardResistanceModifier = section.find("span", {"title": "Resistance Modifier"})
+    
+    if (cardResistance == None):
+        return ({"Resistance": "-"})
+    
+    return ({"Resistance": str(cardResistance["title"]) + " " + str(cardResistanceModifier.text)})
+
+def retrieveRetreat(pageContent):
+    cardRetreatCost = pageContent.find("span", {"title": "Retreat Cost"})
+
+    if (cardRetreatCost != None):
+        return ({"Retreat": (cardRetreatCost.text).split(" ")[1]})
+    
+    return ({"Retreat": "-"})
+
 def retrieveArtist(pageContent):
     cardArtist = pageContent.find("a", {"title": "Illustrator"})
     return ({"Artist": cardArtist.text})
@@ -77,6 +124,13 @@ def retrieveReleaseDate(pageContent):
     cardReleaseDate = pageContent.find("span", {"title": "Date Released"})
     return ({"Release Date": (cardReleaseDate.text)})
 
+def retrieveRulingText(pageContent):
+    cardRules = pageContent.find("div", {"class": "rules minor-text"})
+
+    if (cardRules != None):
+        return ({"Flavor Text": cardRules.text})
+    return ({"Flavor Text": "-"})   
+
 def retrieveFlavorText(pageContent):
     cardFlavor = pageContent.find("div", {"class": "flavor minor-text"})
 
@@ -84,9 +138,24 @@ def retrieveFlavorText(pageContent):
         return ({"Flavor Text": cardFlavor.text})
     return ({"Flavor Text": "-"})
 
+def retrieveMark(pageContent):
+    cardMark = pageContent.find("span", {"class": "Regulation Mark"})
+    return ({"Mark": (cardMark.text).split(" ")[1]})
+
+def retrieveFormats(pageContent):
+    formatString = ""
+    formats = pageContent.find_all("span", {"title": "Format Type"})
+
+    for element in formats:
+        formatString += str(element.text) + ", "
+
+    return ({"Formats": formatString[:-2]})
+
 def retrieveImage(pageContent):
     cardImage = pageContent.find("a")
     return ({"Card Art": cardImage["href"]})
+
+# Reading Overall Data Functions
 
 def readCardInfo(pageContent):
     cardDictionary = {}
@@ -100,19 +169,24 @@ def readCardInfo(pageContent):
     cardDictionary.update(retrieveType(textInfo))
     cardDictionary.update(retrieveSubtype(textInfo))
     cardDictionary.update(retrieveStage(textInfo))
+    cardDictionary.update(retrieveClass(textInfo))
     # EVOLUTIONS (2 of them)
-    # Weakenss, resistance, retreat
+    cardDictionary.update(retrieveWeakness(textInfo))
+    cardDictionary.update(retrieveResistance(textInfo))
+    cardDictionary.update(retrieveRetreat(textInfo))
     # Moves
-    # Formats
-    cardDictionary.update(retrieveArtist(textInfo))
-    cardDictionary.update(retrieveSeries(textInfo))
-    cardDictionary.update(retrieveSet(textInfo))
-    cardDictionary.update(retrieveSetCode(textInfo))
-    cardDictionary.update(retrieveCardID(textInfo))
-    cardDictionary.update(retrieveRarity(textInfo))
-    cardDictionary.update(retrieveReleaseDate(textInfo))
-    cardDictionary.update(retrieveFlavorText(textInfo))
-    cardDictionary.update(retrieveImage(imageInfo))
+    # cardDictionary.update(retrieveArtist(textInfo))
+    # cardDictionary.update(retrieveSeries(textInfo))
+    # cardDictionary.update(retrieveSet(textInfo))
+    # cardDictionary.update(retrieveSetCode(textInfo))
+    # cardDictionary.update(retrieveCardID(textInfo))
+    # cardDictionary.update(retrieveRarity(textInfo))
+    # cardDictionary.update(retrieveReleaseDate(textInfo))
+    cardDictionary.update(retrieveRulingText(textInfo))
+    # cardDictionary.update(retrieveFlavorText(textInfo))
+    cardDictionary.update(retrieveMark(textInfo))
+    cardDictionary.update(retrieveFormats(textInfo))
+    # cardDictionary.update(retrieveImage(imageInfo))
     print(cardDictionary)
 
 def readSetInfo(pageURL):
