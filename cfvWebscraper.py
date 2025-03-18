5bb8bbccfb3d54b4e4c08fe6ef2ec2bbaef9ba62
Ryan Ho <ryanhohoho6@gmail.com>
2025-03-17 23:11:27 -0400

    commented out a few lines in relation to old database file

--- cfvWebscraper.py
+++ cfvWebscraper.py
@@ -261,22 +261,22 @@ def cfvScrapeSet(pageURL):
     setInfo = setPageData.find("table")
     print(setInfo)
 
-def cfvMain():
-    columnNames = ["Card ID", "Name", "Card Type", "Grade", "Skill",
-                    "Imaginary Gift", "Special Icon", "Trigger Effect",
-                    "Power", "Shield", "Critical", "Nation", "Clan", "Race", 
-                    "Series", "Format", "Artist", "Card Effect(s)", 
-                    "Set ID", "Set Name", "Rarity", "Card Art(s)", "Release Date", 
-                    "Language", "Restrictions", "Full Art(s)"]
-    createExcel("cfvdatabase.xlsx", "All Cards", columnNames)
+# def cfvMain():
+#     columnNames = ["Card ID", "Name", "Card Type", "Grade", "Skill",
+#                     "Imaginary Gift", "Special Icon", "Trigger Effect",
+#                     "Power", "Shield", "Critical", "Nation", "Clan", "Race", 
+#                     "Series", "Format", "Artist", "Card Effect(s)", 
+#                     "Set ID", "Set Name", "Rarity", "Card Art(s)", "Release Date", 
+#                     "Language", "Restrictions", "Full Art(s)"]
+#     createExcel("cfvdatabase.xlsx", "All Cards", columnNames)
 
-    list = cfvReadCard("https://cardfight.fandom.com/wiki/Blaster_Blade")
-    updateExcel("cfvdatabase.xlsx", "All Cards", list)
-    # sortExcel("cfvdatabase.xlsx", "Language Sorted", "Language")
-    # updateExcel("cfvdatabase.xlsx", "All Cards", list)
-    # sortExcel("cfvdatabase.xlsx", "Name Sorted", "Name")
+#     list = cfvReadCard("https://cardfight.fandom.com/wiki/Blaster_Blade")
+#     updateExcel("cfvdatabase.xlsx", "All Cards", list)
+#     # sortExcel("cfvdatabase.xlsx", "Language Sorted", "Language")
+#     # updateExcel("cfvdatabase.xlsx", "All Cards", list)
+#     # sortExcel("cfvdatabase.xlsx", "Name Sorted", "Name")
 
-    # readSet()
+#     # readSet()
 
 
 
