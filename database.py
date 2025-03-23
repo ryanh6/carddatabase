7dc93965baaf8b448da1c394a137e617f9912422
unknown <ryanhohoho6@gmail.com>
2025-03-23 15:57:54 -0400

    python doesnt work on pc, moving work to laptop by git

--- database.py
+++ database.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 def dataframe(dictionary):
     return pd.DataFrame.from_dict(dictionary)
+
+def updateFile(newDatabase, name):
+    currentDatabase = pd.read_excel(name)
+    updatedDatabase = currentDatabase.append(newDatabase, ignore_index = True)
+    updatedDatabase.to_excel(name, index = False)
