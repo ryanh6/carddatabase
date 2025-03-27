c41faa2201c0ec009b37806a83ed6d2d595bbca9
Ryan Ho <ryanhohoho6@gmail.com>
2025-03-26 23:27:43 -0400

    added the base functions that I am gonna need

--- database.py
+++ database.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
-def dataframe(dictionary):
+def dictionaryToDataframe(dictionary):
     return pd.DataFrame.from_dict(dictionary)
+
+def toExcel(dataframe):
+    print("Filler")
+
+def removeDuplicates(dataframe):
+    print("remove")
+
+def filterDatabase(keyword):
+    print("filter")
+
+def createExcel(fileName, columnNames):
+    print("Hello")
