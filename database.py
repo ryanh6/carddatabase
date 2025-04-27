335406d31a55d90fcd9ce307ea63520ff93f47d6
unknown <ryanhohoho6@gmail.com>
2025-04-26 23:21:49 -0400

    replacing with mongodb related functions

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
-import pandas as pd
+import json
 
-def dictionaryToDataframe(dictionary):
-    return pd.DataFrame.from_dict(dictionary)
+def dictionaryToJSON(card):
+    return json.dumps(card)
 
-def updateFile(newDatabase, name):
-    try:
-        currentDatabase = pd.read_excel(name)
-        updatedDatabase = currentDatabase.append(newDatabase, ignore_index = True)
-        updatedDatabase.to_excel(name, index = False)
-    except:
-        newDatabase.to_excel(name, index = False)
+# def dictionaryToDataframe(dictionary):
+#     return pd.DataFrame.from_dict(dictionary)
+
+# def updateFile(newDatabase, name):
+#     try:
+#         currentDatabase = pd.read_excel(name)
+#         updatedDatabase = currentDatabase.append(newDatabase, ignore_index = True)
+#         updatedDatabase.to_excel(name, index = False)
+#     except:
+#         newDatabase.to_excel(name, index = False)
